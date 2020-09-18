24378078e9ac6ebf1876dbb3ba37e799884e396b
Sergey Bulavin <murdoc3862@gmail.com>
2020-09-18 11:24:40 +0400

    добавил нормалайз

--- src/js/external.js
+++ src/js/external.js
@@ -7,4 +7,5 @@
 @@include('node_modules/select2/dist/js/select2.full.min.js')
 @@include('node_modules/jquery-validation/dist/jquery.validate.min.js')
 @@include('node_modules/jquery-ui-dist/jquery-ui.min.js')
+@@include('external/normalize.js')
 
