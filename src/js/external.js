f0166065c40e9285778154e8b9d41c5bce673222
Sergey Bulavin <murdoc3862@gmail.com>
2020-09-18 12:08:43 +0400

    норм

--- src/js/external.js
+++ src/js/external.js
@@ -1,7 +1,6 @@
 // external scripts
 
 @@include('external/modernizr-custom.js')
-@@include('node_modules/jquery/dist/jquery.min.js')
 @@include('external/ofi.min.js')
 @@include('node_modules/slick-carousel/slick/slick.js')
 @@include('node_modules/select2/dist/js/select2.full.min.js')
