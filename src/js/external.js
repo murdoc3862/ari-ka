b39939059d463fd60420d751ca63c820f7faf5ed
Sergey Bulavin <murdoc3862@gmail.com>
2020-09-18 12:19:51 +0400

    норм

--- src/js/external.js
+++ src/js/external.js
@@ -4,6 +4,6 @@
 @@include('external/ofi.min.js')
 @@include('external/slick.js')
 @@include('external/select2.full.min.js')
-@@include('node_modules/jquery-validation/dist/jquery.validate.min.js')
+@@include('external/jquery.validate.min.js')
 @@include('node_modules/external/jquery.min.js')
 
