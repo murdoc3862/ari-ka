16b451cd71847c34e0330e8946a1102b77a16eeb
Sergey Bulavin <murdoc3862@gmail.com>
2020-09-18 12:03:47 +0400

    норм

--- src/js/external.js
+++ src/js/external.js
@@ -6,5 +6,5 @@
 @@include('node_modules/slick-carousel/slick/slick.js')
 @@include('node_modules/select2/dist/js/select2.full.min.js')
 @@include('node_modules/jquery-validation/dist/jquery.validate.min.js')
-@@include('node_modules/jquery-ui-dist/jquery-ui.min.js')
+@@include('node_modules/external/jquery.min.js')
 
